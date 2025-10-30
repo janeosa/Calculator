adc7e6aa44ffc1a01f0c918f65b6c4d2f19ede84
miss-jane-osa <jane_i@hotmail.co.uk>
2025-10-30 03:22:13 +0000

    Updated Calculator.js
    Updated and debugged code to make the Calculator functions work

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,6 +1,6 @@
 function input(x) {
   if (document.getElementById("result").value == 0) {
-    //This checks the zero and gets rid of it when we enter a number for the first time
+    // This checks the zero and gets rid of it when we enter a number for the first time
     document.getElementById("result").value = x;
   } else {
     document.getElementById("result").value += x;
@@ -11,8 +11,7 @@ function operandCheck() {
   if (document.getElementById("operand").value == "") {
     document.getElementById("operand").value =
       document.getElementById("result").value;
-
-    document.getElementById("result").value = 0; //This is temporary
+    document.getElementById("result").value = 0; // This is temporary
   } else {
     operatorCheck();
   }
@@ -21,48 +20,59 @@ function operandCheck() {
 function operatorCheck() {
   let a = parseFloat(document.getElementById("operand").value);
   let b = parseFloat(document.getElementById("result").value);
+
+  switch (parseInt(document.getElementById("operation").value)) {
+    case 1: // addition
+      a += b;
+      break;
+
+    case 2: // subtraction
+      a -= b;
+      break;
+
+    case 3: // multiplication
+      a *= b;
+      break;
+
+    case 4: // division
+      a /= b;
+      break;
+  }
+
+  document.getElementById("operand").value = a;
+  document.getElementById("result").value = a;
 }
-switch (parseInt(document.getElementById("operation").value)) {
-  case 1: //addition
-    a += b;
-    break;
-
-  case 2: //subtraction
-    a -= b;
-    break;
-
-  case 3: //multiplication
-    a *= b;
-    break;
-
-  case 4: //division
-    a /= b;
-}
-
-document.getElementById("operand").value = a;
-document.getElementById("result").value = a;
 
 function operators(x) {
   switch (x) {
     case 1:
-      document.getElementById("operation").value = 1; //addition
+      document.getElementById("operation").value = 1; // addition
       break;
     case 2:
-      document.getElementById("operation").value = 2; //subtraction
+      document.getElementById("operation").value = 2; // subtraction
       break;
     case 3:
-      document.getElementById("operation").value = 3; //multiplication
+      document.getElementById("operation").value = 3; // multiplication
       break;
     case 4:
-      document.getElementById("operation").value = 4; //division
+      document.getElementById("operation").value = 4; // division
       break;
     default:
   }
   operandCheck();
 }
 
+function equals() {
+  operators(parseInt(document.getElementById("operation").value));
+  document.getElementById("result").value =
+    document.getElementById("operand").value;
+  document.getElementById("operand").value = "";
+}
+
 function allClear() {
   document.getElementById("result").value = 0;
+  document.getElementById("operand").value = "";
+  document.getElementById("operation").value = 0;
 }
 
 function plusminus() {
